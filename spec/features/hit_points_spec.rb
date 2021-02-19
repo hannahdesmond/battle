10cb4970f6aeb9201db81dbb980b5b624b75743e
Hannah Desmond <desmond.hannah@googlemail.com>
2021-02-19 08:48:15 +0000

    Add web helpers to DRY out feature tests

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'see hit points' do
   scenario 'displays hit points of player 2' do
-    visit('/')
-    fill_in(:player_1_name, with: 'Katie Whitlock')
-    fill_in(:player_2_name, with: 'Hannah Desmond')
-    click_button('Submit')
+    sign_in_and_play
     expect(page).to have_content('Hannah Desmond: 60HP')
   end
 end
--- spec/features/names_spec.rb
+++ spec/features/names_spec.rb
@@ -1,9 +1,6 @@
 feature 'enter names' do
   scenario 'receives player name through form' do
-    visit('/')
-    fill_in('player_1_name', with: 'Katie Whitlock')
-    fill_in('player_2_name', with: 'Hannah Desmond')
-    click_button "Submit"
+    sign_in_and_play
     expect(page).to have_content 'Katie Whitlock vs Hannah Desmond'
   end
 end
